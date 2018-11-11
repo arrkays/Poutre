392ea3fd0ac2e280e5cd50e28d1f91166c55c32e
arrkays <eva.etupal@gmail.com>
2018-11-12 00:42:08 +0100

    communication IN et OUT marche code a réorganiser et a commenter

--- app/src/main/java/com/arrkays/poutre/MainActivity.java
+++ app/src/main/java/com/arrkays/poutre/MainActivity.java
@@ -30,6 +30,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.security.Permission;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
@@ -41,6 +42,12 @@ public class MainActivity extends AppCompatActivity {
     BluetoothAdapter mBluetoothAdapter = null;
     String TAG = "debug-bluetooth";
     MainActivity ma = this;
+    BluetoothGatt bluetoothGatt = null;
+
+    final UUID serviceUUID = UUID.fromString("0000ffe0-0000-1000-8000-00805f9b34fb");
+    final UUID charUUID = UUID.fromString("ffe1-0000-1000-8000-00805f9b34fb");
+    final UUID configChar = UUID.fromString("00002901-0000-1000-8000-00805f9b34fb");
+
     //VIEW
     Graph graph = null;
     TextView monPoid = null;
@@ -82,6 +89,16 @@ public class MainActivity extends AppCompatActivity {
             public boolean onTouch(View v, MotionEvent event) {
                 //BTConnexion.startConnexionBT(ma);
                 ble();
+                return false;
+            }
+        });
+
+        record.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                BluetoothGattCharacteristic characteristic = bluetoothGatt.getService(serviceUUID).getCharacteristic(charUUID);
+                characteristic.setValue(new byte[] {65,65,65,65});
+                bluetoothGatt.writeCharacteristic(characteristic);
                 return false;
             }
         });
@@ -146,6 +163,7 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("debug-bluetooth", "device paired: ");
                 // There are paired devices. Get the name and address of each paired device.
                 for (BluetoothDevice device : pairedDevices) {
+                    Log.d("debug-bluetooth", device.getName());
                     if (device.getName().equals("POUTRE")) {
                         board = device;
                     }
@@ -153,10 +171,6 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-
-        final UUID serviceUUID = convertFromInteger(0xFFE0);
-        final UUID charUUID = convertFromInteger(0xFFE1);
-        final UUID configChar = convertFromInteger(0x2902);
 
         if(board != null){
 
@@ -176,7 +190,7 @@ public class MainActivity extends AppCompatActivity {
                 // New services discovered
                 public void onServicesDiscovered(BluetoothGatt gatt, int status) {
 
-                    Log.d(TAG, "callback onServicesDiscovered ");
+                    /*Log.d(TAG, "callback onServicesDiscovered ");
                     Log.d(TAG, "list of Services: ");
                     for(BluetoothGattService e : gatt.getServices()){
                         Log.d(TAG, e.toString());
@@ -195,28 +209,26 @@ public class MainActivity extends AppCompatActivity {
                                 gatt.setCharacteristicNotification(c, true);
                             }
                         }
-                    }
+                    }*/
 
 
-                    /*BluetoothGattCharacteristic characteristic = gatt.getService(serviceUUID).getCharacteristic(charUUID);
+                    BluetoothGattCharacteristic characteristic = gatt.getService(serviceUUID).getCharacteristic(charUUID);
                     BluetoothGattDescriptor descriptor = characteristic.getDescriptor(configChar);
+                    gatt.setCharacteristicNotification(characteristic, true);
 
-                    Log.d(TAG,"set descriptot to ENABLE_NOTIFICATION_VALUE");
+                    //input
                     descriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
-                    Log.d(TAG,"desc val : "+descriptor.getValue());
-                   // descriptor.setValue(BluetoothGattDescriptor.ENABLE_INDICATION_VALUE);
-                   // Log.d(TAG,"desc val : "+descriptor.getValue());*/
-                    //gatt.writeDescriptor(descriptor);
-                }
+                    gatt.writeDescriptor(descriptor);
+
+                    //output
+                    BluetoothGattDescriptor descriptorOut = characteristic.getDescriptor(UUID.fromString("00002902-0000-1000-8000-00805f9b34fb"));
+                    descriptorOut.setValue(BluetoothGattDescriptor.ENABLE_INDICATION_VALUE);
+                    gatt.writeDescriptor(descriptorOut);
+                  }
 
                 @Override
                 public void onDescriptorWrite(BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status) {
                     Log.d(TAG,"onDescriptorWrite");
-
-                    /*BluetoothGattCharacteristic characteristic = gatt.getService(serviceUUID).getCharacteristic(charUUID);
-                    BluetoothGattDescriptor descriptor2 = characteristic.getDescriptor(convertFromInteger(0x2901));
-                    descriptor2.setValue(new byte[]{1, 1});
-                    gatt.writeDescriptor(descriptor2);*/
                 }
 
                 @Override
@@ -233,7 +245,6 @@ public class MainActivity extends AppCompatActivity {
 
                 @Override
                 public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
-                    Log.d(TAG,"Characteristique: "+characteristic.getUuid());
                     String msg="";
                     for(byte b : characteristic.getValue()){
 
@@ -243,128 +254,8 @@ public class MainActivity extends AppCompatActivity {
                 }
             };
 
-
-            BluetoothGatt mBluetoothGatt = board.connectGatt(this, true, mGattCallback);
-        }
-
-
-    }
-
-    public UUID convertFromInteger(int i) {
-        final long MSB = 0x0000000000001000L;
-        final long LSB = 0x800000805f9b34fbL;
-        long value = i & 0xFFFFFFFF;
-        return new UUID(MSB | (value << 32), LSB);
-    }
-
-
-
-    void bluetoothClient(){
-        if (mBluetoothAdapter != null) {
-            Log.d(TAG, "bluetooth activé");
-            Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-            if (pairedDevices.size() > 0) {
-                Log.d("debug-bluetooth", "device paired: ");
-                // There are paired devices. Get the name and address of each paired device.
-                for (BluetoothDevice device : pairedDevices) {
-                    if(device.getName().equals("POUTRE")) {
-                        board = device;
-
-                        Log.d("debug-bluetooth", "name : " + board.getName());
-                        Log.d("debug-bluetooth", "bond state : " + board.getBondState());
-                        Log.d("debug-bluetooth", "type : " + board.getType());
-                        Log.d("debug-bluetooth", "uuids : " + board.getUuids());
-                        Log.d("debug-bluetooth", "Mac Adresse : " + board.getAddress());
-                        Log.d("debug-bluetooth", "------------------------");
-                    }
-                }
-            }
-        }
-        else
-        {
-            Log.d("debug-bluetooth", "no bluetooth adaptater found");
-        }
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                Log.d("debug-bluetooth", "thread start");
-                Log.d("debug-bluetooth", "Mac Adresse : "+board.getAddress());
-                Log.d("debug-bluetooth", "fetch uuid : "+board.fetchUuidsWithSdp());
-                Log.d("debug-bluetooth", "get uuid : "+board.getUuids());
-
-                try {
-                    byte uuid[] = {0,0};
-                    BluetoothSocket socket = board.createInsecureRfcommSocketToServiceRecord(UUID.fromString("0000FFE0-0000-1000-8000-00805F9B34FB"));
-                    Log.d("debug-bluetooth", "socket init");
-                    Log.d("debug-bluetooth", "connecting socket...");
-                    mBluetoothAdapter.cancelDiscovery();
-                    socket.connect();
-                    Log.d("debug-bluetooth", "socket connecter!");
-                    BTServices = new MyBluetoothService(socket);
-                }catch(IOException e){
-                    e.getCause();
-                    Log.d("debug-bluetooth", "cant open socket");
-
-                };
-            }
-        }).start();
-
-    }
-
-    /*
-    private class AcceptThread extends Thread {
-        private final BluetoothServerSocket mmServerSocket;
-
-        public AcceptThread() {
-            // Use a temporary object that is later assigned to mmServerSocket
-            // because mmServerSocket is final.
-            BluetoothServerSocket tmp = null;
-            try {
-                // MY_UUID is the app's UUID string, also used by the client code.
-                byte uuid[] = {0,0};
-                tmp = mBluetoothAdapter.listenUsingRfcommWithServiceRecord("Poutre", UUID.nameUUIDFromBytes(uuid));
-            } catch (IOException e) {
-                Log.e(TAG, "Socket's listen() method failed", e);
-            }
-            mmServerSocket = tmp;
-        }
-
-        public void run() {
-            BluetoothSocket socket = null;
-            // Keep listening until exception occurs or a socket is returned.
-            while (true) {
-                try {
-                    Log.d(TAG, "Socketserver wait...");
-                    socket = mmServerSocket.accept();
-                } catch (IOException e) {
-                    Log.e(TAG, "Socket's accept() method failed", e);
-                    break;
-                }
-
-                if (socket != null) {
-                    // A connection was accepted. Perform work associated with
-                    // the connection in a separate thread.
-                    Log.d(TAG, "Socket's accept!!!");
-                    new MyBluetoothService(socket);
-                    try {
-                        mmServerSocket.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    break;
-                }
-            }
-        }
-
-        // Closes the connect socket and causes the thread to finish.
-        public void cancel() {
-            try {
-                mmServerSocket.close();
-            } catch (IOException e) {
-                Log.e(TAG, "Could not close the connect socket", e);
-            }
+            bluetoothGatt = board.connectGatt(this, true, mGattCallback);
         }
     }
-    */
 }
 
